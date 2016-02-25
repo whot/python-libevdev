cc6bd8fc7632cb2b57bc8e75c57b651b21454cff
Peter Hutterer <peter.hutterer@who-t.net>
2016-02-25 20:02:51 +1000

    Add context-specific handling, including autogenerated setters and getters
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- libevdev/__init__.py
+++ libevdev/__init__.py
@@ -36,7 +36,26 @@ class _LibraryWrapper(object):
             #   "restype": c_void_p, # return type
             #   "restype": c_void_p, # return type
             #   "name" : "foo", # override default name taken from libevdev
+            # If a type is specified, it can be:
+            #   "getter" ... automatically makes the function an attribute
+            #   "setter" ... automatically makes the function setable attribute
+            #   "type" : <type>
             # }
+    }
+
+    # List of all API calls starting with libevdev_get_. These are mapped
+    # into attributes (see __getattr__), libevdev_get_name becomes
+    # foo = Libevdev().name
+    _getters = {
+            # name : func
+    }
+
+    # List of all API calls starting with libevdev_set_. These are mapped
+    # into attributes (see __setattr__), libevdev_set_name becomes
+    # foo = Libevdev()
+    # foo.name = somename
+    _setters = {
+            # name : func
     }
 
     def __init__(self):
@@ -53,9 +72,19 @@ class _LibraryWrapper(object):
             func = getattr(cls._lib, name)
             func.argtypes = attrs["argtypes"]
             func.restype = attrs["restype"]
-            # default name is the libevdev name minus the libevdev_ prefix
-            pyname = dict.get_default(attrs, "name", name[len("libevdev_")])
-            setattr(cls, pyname, func)
+            # getters and setters have special treatment
+            # http://stackoverflow.com/questions/412951/how-to-implement-property-with-dynamic-name-in-python
+            t = attrs.get("type", None)
+            if t == "getter":
+                pyname = name.split("libevdev_get_")[1]
+                cls._getters[pyname] = func
+            elif t == "setter":
+                pyname = name.split("libevdev_set_")[1]
+                cls._setters[pyname] = func
+            else:
+                # default name is the libevdev name minus the libevdev_ prefix
+                pyname = dict.get(attrs, "name", name[len("libevdev_"):])
+                setattr(cls, pyname, func)
 
         return cls._lib
 
@@ -100,11 +129,95 @@ class Libevdev(_LibraryWrapper):
         #const char *libevdev_property_get_name(unsigned int prop);
         "libevdev_property_get_name": {
             "argtypes": (c_uint,),
-            "restype": c_char_p
+            "restype": c_char_p,
             },
         #int libevdev_property_from_name(const char *name);
         "libevdev_property_from_name": {
             "argtypes": (c_char_p,),
             "restype": c_int
             },
+        #struct libevdev *libevdev_new(void);
+        "libevdev_new": {
+            "argtypes": (),
+            "restype": c_void_p,
+            "name" : "_new",
+            },
+        #struct libevdev *libevdev_free(struct libevdev *);
+        "libevdev_free": {
+            "argtypes": (c_void_p,),
+            "restype": None,
+            "name" : "_free",
+            },
+        #const char * libevdev_get_name(struct libevdev *);
+        "libevdev_get_name": {
+            "argtypes": (c_void_p,),
+            "restype": c_char_p,
+            "type" : "getter",
+            },
+        #void libevdev_set_name(struct libevdev *, const char*);
+        "libevdev_set_name": {
+            "argtypes": (c_void_p, c_char_p),
+            "restype": None,
+            "type" : "setter",
+            },
+        #const char * libevdev_get_phys(struct libevdev *);
+        "libevdev_get_phys": {
+            "argtypes": (c_void_p,),
+            "restype": c_char_p,
+            "type" : "getter",
+            },
+        #void libevdev_set_phys(struct libevdev *, const char*);
+        "libevdev_set_phys": {
+            "argtypes": (c_void_p, c_char_p),
+            "restype": None,
+            "type" : "setter",
+            },
+        #const char * libevdev_get_uniq(struct libevdev *);
+        "libevdev_get_uniq": {
+            "argtypes": (c_void_p,),
+            "restype": c_char_p,
+            "type" : "getter",
+            },
+        #void libevdev_set_uniq(struct libevdev *, const char*);
+        "libevdev_set_uniq": {
+            "argtypes": (c_void_p, c_char_p),
+            "restype": None,
+            "type" : "setter",
+            },
+        #int libevdev_get_driver_version(struct libevdev *);
+        "libevdev_get_driver_version": {
+            "argtypes": (c_void_p, ),
+            "restype": c_int,
+            "type" : "getter",
+            },
+        #void libevdev_set_clock_id(struct libevdev *, int)
+        "libevdev_set_clock_id": {
+            "argtypes": (c_void_p, c_int),
+            "restype": None,
+            "type" : "setter",
+            },
         }
+
+    def __init__(self):
+        super(Libevdev, self).__init__()
+        self._ctx = self._new()
+
+    def __del__(self):
+        if hasattr(self, "_ctx"):
+            self._free(self._ctx)
+
+    def __getattr__(self, name):
+        try:
+            return self._getters[name](self._ctx)
+        except KeyError:
+            msg = "'{0}' object has no attribute '{1}'"
+            raise AttributeError(msg.format(type(self).__name__, name))
+
+    def __setattr__(self, name, value):
+        if name.startswith("_"):
+            self.__dict__[name] =  value
+        else:
+            try:
+                self._setters[name](self._ctx, value)
+            except KeyError:
+                self.__setitem__(name, value)

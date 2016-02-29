5ab1a98508818be3035d6b77ae2e3986f2d08787
Peter Hutterer <peter.hutterer@who-t.net>
2016-02-29 10:52:00 +1000

    Add setup.py
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='libevdev',
+        version='0.1',
+        description='Python wrapper for libevdev',
+        author='Peter Hutterer',
+        author_email='peter.hutterer@who-t.net',
+        url='https://www.github.com/whot/libevdev-python/',
+        packages=['libevdev'],
+        )
+

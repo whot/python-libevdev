735428872184174d708811b5f043ba18640e1914
Peter Hutterer <peter.hutterer@who-t.net>
2017-12-11 18:49:36 +1000

    test: add a test case for the new classes
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

---
+++ test/test-const.py
@@ -0,0 +1,74 @@
+# -*- coding: latin-1 -*-
+# Copyright © 2017 Red Hat, Inc.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice (including the next
+# paragraph) shall be included in all copies or substantial portions of the
+# Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+
+import unittest
+
+import libevdev
+from libevdev import evbit, prop
+
+class TestEventBits(unittest.TestCase):
+    def test_ev_types(self):
+        self.assertIn(libevdev.EV_SYN, libevdev.types)
+        self.assertIn(libevdev.EV_REL, libevdev.types)
+        self.assertIn(libevdev.EV_ABS, libevdev.types)
+        self.assertEqual(len(libevdev.types), 13)
+
+    def test_EV_REL(self):
+        self.assertIn(libevdev.EV_REL.REL_X, libevdev.EV_REL.codes)
+        self.assertIn(libevdev.EV_REL.REL_Y, libevdev.EV_REL.codes)
+        self.assertNotIn(libevdev.EV_ABS.ABS_X, libevdev.EV_REL.codes)
+
+    def test_type_max(self):
+        self.assertEqual(libevdev.EV_REL.max, libevdev.EV_REL.REL_MAX.value)
+        self.assertEqual(libevdev.EV_ABS.max, libevdev.EV_ABS.ABS_MAX.value)
+        self.assertEqual(libevdev.EV_KEY.max, libevdev.EV_KEY.KEY_MAX.value)
+
+        self.assertEqual(libevdev.EV_REL.max, 0x0f)
+        self.assertEqual(libevdev.EV_ABS.max, 0x3f)
+        self.assertEqual(libevdev.EV_KEY.max, 0x2ff)
+
+    def test_evcode_compare(self):
+        self.assertNotEqual(libevdev.EV_REL.REL_X, libevdev.EV_REL.REL_Y)
+        self.assertNotEqual(libevdev.EV_REL.REL_X, libevdev.EV_ABS.ABS_X)
+        self.assertNotEqual(libevdev.EV_REL, libevdev.EV_ABS)
+
+        self.assertNotEqual(libevdev.EV_REL.REL_X, libevdev.EV_REL)
+        self.assertNotEqual(libevdev.EV_ABS.ABS_X, libevdev.EV_ABS)
+
+    def test_evbit(self):
+        self.assertEqual(evbit(0, 0), libevdev.EV_SYN.SYN_REPORT)
+        self.assertEqual(evbit(1, 30), libevdev.EV_KEY.KEY_A)
+        self.assertEqual(evbit(2, 1), libevdev.EV_REL.REL_Y)
+
+        self.assertEqual(evbit(0), libevdev.EV_SYN)
+        self.assertEqual(evbit(1), libevdev.EV_KEY)
+        self.assertEqual(evbit(2), libevdev.EV_REL)
+
+        for t in libevdev.types:
+            self.assertEqual(evbit(t.value), t)
+
+    def test_propbit(self):
+        self.assertEqual(prop(0), libevdev.INPUT_PROP_POINTER)
+        self.assertEqual(prop(1), libevdev.INPUT_PROP_DIRECT)
+
+        for p in libevdev.props:
+            self.assertEqual(prop(p.value), p)
--- test/test-events.py
+++ test/test-events.py
@@ -1,21 +1,54 @@
-import sys
+# -*- coding: latin-1 -*-
+# Copyright © 2017 Red Hat, Inc.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice (including the next
+# paragraph) shall be included in all copies or substantial portions of the
+# Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
 
-from libevdev import Libevdev
+import unittest
 
+import libevdev
+from libevdev import evbit, prop, InputEvent
 
-def main(argv):
-    device = argv[1]
-    with open(device, "rb") as fd:
-        print("Opened {} in blocking mode, waiting for an event.".format(device))
-        l = Libevdev(fd)
-        while True:
-            ev = l.next_event()
-            if ev.matches("EV_SYN", "SYN_REPORT"):
-                break
+class TestEvents(unittest.TestCase):
+    def test_event_matches_type(self):
+        ev = InputEvent(libevdev.EV_REL)
+        self.assertTrue(ev.matches(libevdev.EV_REL))
+        self.assertFalse(ev.matches(libevdev.EV_REL.REL_X))
+        self.assertFalse(ev.matches(libevdev.EV_ABS))
+        self.assertFalse(ev.matches(libevdev.EV_ABS.ABS_X))
 
+    def test_event_matches_code(self):
+        ev = InputEvent(libevdev.EV_REL.REL_X)
+        self.assertTrue(ev.matches(libevdev.EV_REL.REL_X))
+        self.assertTrue(ev.matches(libevdev.EV_REL))
+        self.assertFalse(ev.matches(libevdev.EV_ABS))
+        self.assertFalse(ev.matches(libevdev.EV_ABS.ABS_X))
 
-if __name__ == '__main__':
-    if len(sys.argv) <= 1:
-        print("Usage: {} /dev/input/eventX".format(sys.argv[0]))
-        sys.exit(1)
-    main(sys.argv)
+    def test_event_matches_self(self):
+        e1 = InputEvent(libevdev.EV_REL.REL_X)
+        e2 = InputEvent(libevdev.EV_REL)
+        self.assertEqual(e1, e1)
+        self.assertEqual(e2, e2)
+
+        self.assertEqual(e2, e1)
+        self.assertEqual(e1, e2)
+
+        e2 = InputEvent(libevdev.EV_REL.REL_Y)
+        self.assertNotEqual(e2, e1)
+        self.assertNotEqual(e1, e2)

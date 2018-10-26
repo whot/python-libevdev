227ded0821fe0646c0f1246d3c32e3bed2222aff
Peter Hutterer <peter.hutterer@who-t.net>
2018-10-26 11:25:38 +1000

    Bump to version 0.6
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ https://python-evdev.readthedocs.io/en/latest/
 '''
 
 setup(name='libevdev',
-      version='0.5',
+      version='0.6',
       description='Python wrapper for libevdev',
       long_description=long_description,
       author='Peter Hutterer',
